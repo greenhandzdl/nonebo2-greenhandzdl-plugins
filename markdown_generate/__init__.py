75e69bab909e230266760c8af630a3e8680c8422
greenhandzdl <greenhandzdl@gmail.com>
2023-07-28 22:58:07 +0800

    Update __init__.py

--- markdown_generate/__init__.py
+++ markdown_generate/__init__.py
@@ -48,9 +48,10 @@ async def handle_md_generate(bot: Bot, event: Event, state: T_State):
     # 使用Image类的from_bytes方法创建图片消息段
     image_msg = Image.from_bytes(image_data)
     # 使用MessageFactory类构建消息
-    msg = MessageFactory.reply(event.message_id) + MessageFactory.mention(event.user_id) + ([Image(image_msg),Text("这是渲染的图片")]) 
+    msg = MessageFactory([Image(file_path),Text("这是渲染的图片")]) 
     # 使用bot对象发送消息给用户，回复原消息并@用户
-    await msg.send()
+    await msg.send(reply=True, at_sender=True)
+
 
     # 结束命令处理
     await md_generate_cmd.finish()
