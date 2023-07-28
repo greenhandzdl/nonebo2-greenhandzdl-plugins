5d03875170b8b6f2d866644abf52fa4cf9a59dbf
greenhandzdl <greenhandzdl@gmail.com>
2023-07-28 23:39:31 +0800

    Update __init__.py

--- man/__init__.py
+++ man/__init__.py
@@ -4,71 +4,52 @@ from nonebot.adapters import Bot, Event
 from nonebot.rule import to_me
 
 # 导入 markdown, imgkit, nonebot_plugin_saa, os, datetime 等模块
+
 import markdown
 import imgkit
 from nonebot_plugin_saa import MessageFactory, Text, Image
 import os
-import datetime
 
 # 定义一个命令处理器，响应用户输入的 "man" 指令，并且需要 @ 机器人
 man_cmd = on_command("man", rule=to_me(), priority=5)
 
-
 # 定义命令处理函数，并回复给用户
 @man_cmd.handle()
 async def handle_man(bot: Bot, event: Event, state: T_State):
-    # 获取用户输入的参数
-    param = event.get_plaintext().strip()
-    param = param.replace("/man", "").strip()
+    # 获取用户输入的参数，如果没有输入参数，使用默认的 README.md 文件
+    param = event.get_plaintext().strip().replace("/man", "").strip() or "README"
 
-    # 如果没有输入参数，使用默认的 README.md 文件
-    if not param:
-        md_file = "./src/plugins/README.md"
+    # 拼接成 ./src/plugins/<param>/README.md 文件，并转换为绝对路径
+    md_file = os.path.abspath(f"./src/plugins/{param}/README.md")
 
-    # 否则，拼接成 ./src/plugins/<param>/README.md 文件
-    else:
-        md_file = f"./src/plugins/{param}/README.md"
-
-    # 转换为绝对路径
-    md_file = os.path.abspath(md_file)
-
-    # 检查文件是否存在，如果不存在，提示用户
+    # 检查文件是否存在，如果不存在，提示用户并结束命令处理
     if not os.path.exists(md_file):
         await bot.send(event, "没有找到对应的文件，请检查你输入的参数是否正确")
-        return
+        return await man_cmd.finish()
 
-    # 读取文件内容，并转换为 markdown 文本
+    # 读取文件内容，并转换为 markdown 文本和 html 文本
     with open(md_file, "r", encoding="utf-8") as f:
         md_text = f.read()
-
-    # 调用 markdown 模块的 markdown 函数，将 markdown 文本转换为 html 文本
     html_text = markdown.markdown(md_text)
 
-    # 拼接成一个文件名
+    # 拼接成一个文件名和文件路径，使用 ./cookie 目录和文件名，并转换为绝对路径
     file_name = f"{param}_manual.png"
+    file_path = os.path.abspath(os.path.join("./cookie", file_name))
 
     # 检查并创建 ./cookie 文件夹，如果不存在的话
-    cookie_dir = "./cookie"
-    if not os.path.exists(cookie_dir):
-        os.mkdir(cookie_dir)
+    os.makedirs("./cookie", exist_ok=True)
 
-    # 拼接一个文件路径，使用 ./cookie 目录和文件名，并转换为绝对路径
-    file_path = os.path.join(cookie_dir, file_name)
-    file_path = os.path.abspath(file_path)
-
-    # 调用 imgkit 模块的 from_string 函数，将 html 文本转换为 png 图片，并保存到文件路径中
+    # 将 html 文本转换为 png 图片，并保存到文件路径中
     imgkit.from_string(html_text, file_path)
 
-    # 读取文件路径中的字节数据
+    # 读取文件路径中的字节数据，并创建图片消息段
     with open(file_path, "rb") as f:
         image_data = f.read()
-    # 使用Image类的from_bytes方法创建图片消息段
-    # image_msg = Image.from_bytes(image_data)
-    # 使用MessageFactory类构建消息，并添加一些文本说明
-    msg = MessageFactory([Text("这是我根据你输入的参数生成的说明：\n"), Image(image_data)])
+    image_msg = Image(image_data)
 
-    # 使用 bot 对象发送消息给用户，回复原消息并 @ 用户
+    # 构建消息，并添加一些文本说明
+    msg = MessageFactory([Text("这是我根据你输入的参数生成的说明：\n"), image_msg])
+
+    # 发送消息给用户，回复原消息并 @ 用户，并结束命令处理
     await msg.send(reply=True, at_sender=True)
-
-    # 结束命令处理
     await man_cmd.finish()
