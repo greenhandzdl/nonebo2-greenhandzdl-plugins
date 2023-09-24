902391ef7fd43217d186f41fb6eedf6c353da95d
greenhandzdl <greenhandzdl@gmail.com>
2023-09-24 16:56:02 +0800

    fix

--- random_dice/__init__.py
+++ random_dice/__init__.py
@@ -1,37 +1,47 @@
-# 导入nonebot2相关模块
 from nonebot import on_command
 from nonebot.typing import T_State
 from nonebot.adapters import Bot, Event
 from nonebot.rule import to_me
 
-# 导入随机数模块
 import random
+from nonebot_plugin_saa import Config, on_startup
 
-# 导入插件配置
-from .config import Config
-
-# 导入nonebot-plugin-send-anything-anywhere相关模块
-from nonebot_plugin_saa import MessageFactory, Text
-
+random_cmd = on_command("random", rule=to_me(), priority=5)
 
 # 获取插件配置
 global_config = Config()
 
-# 定义一个命令处理器，响应用户输入的"random"或"随机数"指令，并且需要@机器人
-random_cmd = on_command("random", aliases={"随机数"}, rule=to_me(), priority=5)
-
-# 定义命令处理函数，生成一个1-10的随机数，并回复给用户
 @random_cmd.handle()
 async def handle_random(bot: Bot, event: Event, state: T_State):
+    param = event.get_plaintext().strip()
+    param = param.replace("/random", "").strip()
 
-    # 生成一个1-10的随机整数
-    num = random.randint(global_config.random_dice_min, global_config.random_dice_max)
+    # 检查参数是否为空
+    if not param:
+        await bot.send(event, "请输入最小值、最大值和投掷次数，格式为：/random 最小值 最大值 投掷次数")
+        return
 
-    # 构建一个消息对象，包含文字和at
-    msg = MessageFactory([Text(f"你要的随机数是：{num}")])
+    # 提取最小值、最大值和投掷次数
+    try:
+        min_value, max_value, throw_count = map(int, param.split())
+    except ValueError:
+        await bot.send(event, "参数格式不正确，请输入整数值")
+        return
 
-    # 直接调用send方法在handler中回复消息，自动适配不同的adapter
-    await msg.send(reply=True, at_sender=True)
 
-    #结束命令处理
+    # 检查投掷次数是否超过限制
+    if throw_count > global_config.random_max_count:
+        await bot.send(event, f"投掷次数不能超过{global_config.random_max_count}")
+        return
+
+    # 生成随机数
+    msg = ""
+    for i in range(throw_count):
+        random_number = random.randint(min_value, max_value)
+        msg += f"第{i+1}次，你投出的点数为：{random_number}\n"
+
+    # 发送消息给用户
+    await bot.send(event, msg)
+
+    # 结束命令处理
     await random_cmd.finish()
--- random_dice/config.py
+++ random_dice/config.py
@@ -3,9 +3,7 @@ from pydantic import BaseSettings
 
 # 定义插件配置类
 class Config(BaseSettings):
-    # 随机数最小值，默认为1
-    random_dice_min: int = 1
-    # 随机数最大值，默认为10
-    random_dice_max: int = 10
+    # 限制一次性随机多少
+    random_max_count = 3
 
 global_config = Config()
