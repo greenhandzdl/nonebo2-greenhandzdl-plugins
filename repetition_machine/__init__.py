3e66db4a04e9efe2a61808cb8bec15a046e52b82
greenhandzdl <greenhandzdl@gmail.com>
2023-09-29 01:53:20 +0800

    Update __init__.py

--- repetition_machine/__init__.py
+++ repetition_machine/__init__.py
@@ -19,6 +19,6 @@ async def repeat_message(bot: Bot, event: Event):
     }
 
     if repeatInt >= repeatHitInt:
-        repeat_text = Text(user_input)
-        reply_message = MessageFactory.create([repeat_text])
+        repeat_text = Text(f"${user_input}")
+        reply_message = MessageFactory(repeat_text)
         await bot.send(event, message=reply_message)
