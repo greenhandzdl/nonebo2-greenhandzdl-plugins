5a4f6e2c324732b458ff3df03fd6dfec04c86bf3
greenhandzdl <greenhandzdl@gmail.com>
2023-09-29 01:32:40 +0800

    Update config.py

--- repetition_machine/config.py
+++ repetition_machine/config.py
@@ -1,8 +1,7 @@
-# 导入nonebot2相关模块
-from pydantic import BaseSettings
+from pydantic import BaseModel, Extra
 
 
-class Config(BaseSettings):
+class Config(BaseModel, extra=Extra.ignore):
     """Plugin Config Here"""
     repeatInt : int = 100
     repeatHitInt : int = 40
