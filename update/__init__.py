597e48f785533570d316d2ebf72b6672fef508a0
greenhandzdl <greenhandzdl@gmail.com>
2023-09-29 00:48:36 +0800

    Update __init__.py

--- update/__init__.py
+++ update/__init__.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-from nonebot import on_command
+from nonebot import on_command, get_driver
 from nonebot.typing import T_State
 from nonebot.adapters import Bot, Event
 from nonebot.rule import to_me
@@ -20,12 +20,23 @@ async def handle_update(bot: Bot, event: Event, state: T_State):
     
     # 判断是否有更新
     if "Already up to date" in output:
-        msg = MessageFactory(Text("插件无更新"))
+        msg = MessageFactory(Text("无更新"))
     else:
         # 获取最新的commit信息
         commit_output = subprocess.getoutput("git log -1 --pretty=format:'%h - %s (%cr)'")
         msg = MessageFactory(Text(f"插件已更新！\n最新的commit信息：\n{commit_output}"))
+        
+        # 重启机器人
+        restart_bot()
     
     os.chdir(current_dir)
     await msg.send(reply=True, at_sender=True)
     await update_cmd.finish()
+
+def restart_bot():
+    # 获取机器人的配置
+    global_config = get_driver().config
+    config = Config.parse_obj(global_config)
+    
+    # 重启机器人
+    os.execv(config.cli_path, [config.cli_path, *config.cli_args])
